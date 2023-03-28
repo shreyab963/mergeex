7c8f97c196dc38e2e0471681b2f20a5445f35a8d
shreyabhatta <shreya.bhatta@mavs.uta.edu>
2023-03-28 11:37:16 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# CSE-3311 PROJECT: **HALETH** by Team 3
+
+## Team 3 Developers:
+
+Henry Tran, Mohammed Rayed, Shreya Bhatta, Bhumika Shrestha, Sandeep Adhikari
+
+## Description:
+
+A convenient mobile application that encourages a healthy lifestyle one day at a time. The app will have a unique student-friendly design including a wide range of major services, key features, and additional options.
+
+## Details:
+
+Android Studio Electric Eel 2022.1.1 Patch 1  
+API 33 Android 13.0 (TIRAMISU)  
+Language: Java 19.0.2 Oracle OpenJDK  
+Gradle: 7.6  
+Android Gradle Plugin 7.4.1  
+
+Emulators Used: Pixel 2 API 33 Android 13.0 Google Play | x86_64  
+
+## Features / Activities:
+
+Register, Login, Alarm, Calendar, Cookbook, Fitness Profile 
+
+## Tools / Plugins / Libraries:
+
+Android VCS - JetBrains IDE Integration with Git  
+Firebase Authentication  
+Firebase Realtime Database  
+
+## To run the app:
+Download the zip file of the repository and open it in Android studio. Download an emulator to your Android studio and select it under the devices. Proceed by hitting run.
+
+Common Errors Here:  
+Execution failed for task ':app:compileDebugJavaWithJavac'   Cause: error: invalid source release: 19  
+File > Settings > Build, Execution, Deployment > Build Tools > Gradle > Select Gradle JDK to 19 [Oracle OpenJDK 19.0.2]  
+
+## Additional Notes:
+
+University of Texas at Arlington (UTA)  
+College of Engineering  
+CSE-3311-002 SPR-2023  
+Instructor: Soumik Mohian  
+TA: Sabrina Haque  
+
+# END
---
+++ app/src/main/java/com/example/signuploginfirebase/LoginActivity.java
@@ -0,0 +1,92 @@
+package com.example.signuploginfirebase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    private EditText loginEmail, loginPassword;
+    private TextView signupRedirectText;
+    private Button loginButton;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("users");
+        loginEmail = findViewById(R.id.login_email);
+        loginPassword = findViewById(R.id.login_password);
+        loginButton = findViewById(R.id.login_button);
+        signupRedirectText = findViewById(R.id.signupRedirectText);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = loginEmail.getText().toString();
+                String pass = loginPassword.getText().toString();
+
+                if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    if (!pass.isEmpty()) {
+                        auth.signInWithEmailAndPassword(email, pass)
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        String uid = auth.getCurrentUser().getUid();
+                                        DatabaseClass databaseClass = new DatabaseClass(email, pass);
+                                        databaseReference.child(uid).setValue(databaseClass);
+                                        Toast.makeText(LoginActivity.this, "Login Successful! ", Toast.LENGTH_LONG).show();
+                                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                        finish();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(LoginActivity.this, "Login Attempt Failed.", Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                    } else {
+                        loginPassword.setError("Password cannot be empty");
+                    }
+                } else if (email.isEmpty()) {
+                    loginEmail.setError("Email cannot be empty");
+                } else {
+                    loginEmail.setError("Please enter valid email");
+                }
+            }
+        });
+
+        signupRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+            }
+        });
+
+    }
+}
