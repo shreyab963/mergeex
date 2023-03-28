f0a78b055252848d56fd5584168a3e1b525b6ae9
shreyabhatta <shreya.bhatta@mavs.uta.edu>
2023-03-28 11:35:26 -0500

    Initial commit

---
+++ Downloads/CSE-3311-main 4/app/src/main/java/com/example/signuploginfirebase/DatabaseClass.java
@@ -0,0 +1,30 @@
+package com.example.signuploginfirebase;
+
+public class DatabaseClass {
+
+    String email, pass;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public DatabaseClass(String email, String pass) {
+        this.email = email;
+        this.pass = pass;
+    }
+
+    public DatabaseClass() {
+    }
+}
